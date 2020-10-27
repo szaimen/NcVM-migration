108cf82a0d1ebf79a1dc85a3ee74ef71d19393c4
szaimen <szaimen@e.mail.de>
2020-10-27 01:54:09 +0100

    attempt to fix space issue

--- migration.sh
+++ migration.sh
@@ -642,7 +642,7 @@ echo "Done"
 
 # Appending the new local IP-address to trusted Domains
 echo "appending the new Ip-Address to trusted Domains"
-IP_ADDR=\$(hostname -I | awk '{print $1}')
+IP_ADDR=\$(hostname -I | awk '{print $1}' | sed 's/ //')
 i=0
 while [ "\$i" -le 10 ]; do
     if [ "\$(occ config:system:get trusted_domains "\$i")" = "" ]; then
